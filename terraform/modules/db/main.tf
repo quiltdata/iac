01bbd64b7d709dd867bf4c109f6d05950f24b6e5
Dima Ryazanov <dima@gmail.com>
2023-06-15 19:23:18 -0700

    Remove computed_

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -5,13 +5,12 @@ module "db_accessor_security_group" {
   description = "For resources that need access to DB"
   vpc_id = var.vpc_id
 
-  computed_egress_with_source_security_group_id = [
+  egress_with_source_security_group_id = [
     {
       rule = "postgresql-tcp"
       source_security_group_id = module.db_security_group.security_group_id
     }
   ]
-  number_of_computed_egress_with_source_security_group_id = 1
 }
 
 module "db_security_group" {
@@ -21,13 +20,12 @@ module "db_security_group" {
   description = "For DB resources"
   vpc_id = var.vpc_id
 
-  computed_ingress_with_source_security_group_id = [
+  ingress_with_source_security_group_id = [
     {
       rule = "postgresql-tcp"
       source_security_group_id = module.db_accessor_security_group.security_group_id
     }
   ]
-  number_of_computed_ingress_with_source_security_group_id = 1
 }
 
 module "db" {
