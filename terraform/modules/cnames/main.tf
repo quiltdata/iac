c1ccb92f56922a140132343329462dd154e161f8
Aneesh Karve <aneesh.karve@gmail.com>
2023-07-11 16:09:32 -0700

    Fix record names; add newlines

--- terraform/modules/cnames/main.tf
+++ terraform/modules/cnames/main.tf
@@ -1,15 +1,15 @@
 locals {
-  type = "CNAME"
-  match = regex("(^[^.]+)(\\..*)", var.lb_dns_name)
+  type      = "CNAME"
+  match     = regex("(^[^.]+)(\\..*)", var.quilt_web_host)
   subdomain = local.match[0]
   remainder = local.match[1]
 }
 
 resource "aws_route53_record" "cnames" {
   for_each = toset(["", "-registry", "-s3-proxy"])
-  zone_id = var.zone_id
-  name    = format("%s%s%s", local.subdomain, each.key, local.remainder)
-  type    = local.type
-  ttl     = var.ttl
-  records = [var.lb_dns_name]
+  zone_id  = var.zone_id
+  name     = format("%s%s%s", local.subdomain, each.key, local.remainder)
+  type     = local.type
+  ttl      = var.ttl
+  records  = [var.lb_dns_name]
 }
--- terraform/modules/cnames/variables.tf
+++ terraform/modules/cnames/variables.tf
@@ -1,25 +1,25 @@
 variable "lb_dns_name" {
   description = "DNS name for the Quilt application load balancer"
   type        = string
-  nullable = false
+  nullable    = false
 }
 
 variable "quilt_web_host" {
   description = "QuiltWebHost parameter for stack"
-  type = string
-  nullable = false
+  type        = string
+  nullable    = false
 }
 
 variable "ttl" {
   description = "Record time to live"
-  default = 60
-  type = number
-  nullable = false
+  default     = 60
+  type        = number
+  nullable    = false
 }
 
 variable "zone_id" {
   description = "Hosted zone id"
-  default = "Z08925852XFGELN33FZFQ"
+  default     = "Z08925852XFGELN33FZFQ"
   type        = string
-  nullable = false
+  nullable    = false
 }
