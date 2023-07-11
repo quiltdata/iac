ba45dfa62dd7d494a482546d3f31b1901a354b29
Aneesh Karve <aneesh.karve@gmail.com>
2023-07-11 13:54:27 -0700

    do iteration properly; concision

--- terraform/modules/cnames/main.tf
+++ terraform/modules/cnames/main.tf
@@ -1,32 +1,15 @@
 locals {
   type = "CNAME"
-}
-resource "aws_route53_record" "catalog" {
-  zone_id    = var.zone_id
-  name       = var.parameters["QuiltWebHost"]
-  type       = local.type
-  ttl        = var.ttl
-  records = [
-    var.lb_dns_name
-  ]
+  match = regex("(^[^.]+)(\\..*)", var.lb_dns_name)
+  subdomain = local.match[0]
+  remainder = local.match[1]
 }
 
-resource "aws_route53_record" "registry" {
-  zone_id    = var.zone_id
-  name       = format("%s-registry%s", local.domain_first, local.domain_rest)
-  type       = local.type
-  ttl        = var.ttl
-  records = [
-    var.lb_dns_name
-  ]
-}
-
-resource "aws_route53_record" "s3-proxy" {
-  zone_id    = var.zone_id
-  name       = format("%s-s3-proxy%s", local.domain_first, local.domain_rest)
-  type       = local.type
-  ttl        = var.ttl
-  records = [
-    var.lb_dns_name
-  ]
+resource "aws_route53_record" "cnames" {
+  for_each = toset(["", "-registry", "-s3-proxy"])
+  zone_id = var.zone_id
+  name    = format("%s%s%s", local.subdomain, each.key, local.remainder)
+  type    = local.type
+  ttl     = var.ttl
+  records = [var.lb_dns_name]
 }
