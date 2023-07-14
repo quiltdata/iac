84686346d395d4ec23e7d4bb9de507fbb7bb1761
Aneesh Karve <aneesh.karve@gmail.com>
2023-07-13 17:37:31 -0700

    hardcode CNAME b/c this is the cnames module

--- terraform/modules/cnames/main.tf
+++ terraform/modules/cnames/main.tf
@@ -8,7 +8,6 @@ terraform {
 }
 
 locals {
-  type      = "CNAME"
   match     = regex("(^[^.]+)(\\..*)", var.quilt_web_host)
   subdomain = local.match[0]
   remainder = local.match[1]
@@ -18,7 +17,7 @@ resource "aws_route53_record" "cnames" {
   for_each = toset(["", "-registry", "-s3-proxy"])
   zone_id  = var.zone_id
   name     = format("%s%s%s", local.subdomain, each.key, local.remainder)
-  type     = local.type
+  type     = "CNAME"
   ttl      = var.ttl
   records  = [var.lb_dns_name]
 }
