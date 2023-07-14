4d8259958024f5229e8d95ba480808745cd1cce1
Aneesh Karve <akarve@users.noreply.github.com>
2023-07-14 13:20:05 -0700

    Add a CNAMES module (#12)

---
+++ terraform/modules/cnames/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  # specify the version according to your requirement
+    }
+  }
+}
+
+locals {
+  match     = regex("^([^.]+)(\\..*)$", var.quilt_web_host)
+  subdomain = local.match[0]
+  remainder = local.match[1]
+}
+
+resource "aws_route53_record" "cnames" {
+  for_each = toset(["", "-registry", "-s3-proxy"])
+  zone_id  = var.zone_id
+  name     = format("%s%s%s", local.subdomain, each.key, local.remainder)
+  type     = "CNAME"
+  ttl      = var.ttl
+  records  = [var.lb_dns_name]
+}
---
+++ terraform/modules/cnames/variables.tf
@@ -0,0 +1,24 @@
+variable "lb_dns_name" {
+  description = "DNS name for the Quilt application load balancer"
+  type        = string
+  nullable    = false
+}
+
+variable "quilt_web_host" {
+  description = "QuiltWebHost parameter for stack"
+  type        = string
+  nullable    = false
+}
+
+variable "ttl" {
+  description = "Record time to live"
+  default     = 60
+  type        = number
+  nullable    = false
+}
+
+variable "zone_id" {
+  description = "Hosted zone id"
+  type        = string
+  nullable    = false
+}
