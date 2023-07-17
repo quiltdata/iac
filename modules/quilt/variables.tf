cb8b8987cdeaf2bd76e35e3f133afbd64ef77734
Aneesh Karve <aneesh.karve@gmail.com>
2023-07-17 12:08:14 -0700

    Revert README changes re: domain/cluster

--- modules/quilt/variables.tf
+++ modules/quilt/variables.tf
@@ -48,7 +48,7 @@ variable "search_auto_tune_desired_state" {
   type        = string
   nullable    = false
   default     = "DISABLED"
-  description = "The Auto-Tune desired state for the ElasticSearch cluster"
+  description = "The Auto-Tune desired state for the ElasticSearch domain"
 }
 
 variable "search_instance_count" {
