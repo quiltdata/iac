7117ce5185ce63ee793e88bec80fb6974ba837dc
Aneesh Karve <aneesh.karve@gmail.com>
2023-10-18 17:09:31 -0700

    fmt

--- modules/quilt/main.tf
+++ modules/quilt/main.tf
@@ -30,8 +30,8 @@ module "db" {
 
   network_type = var.db_network_type
 
-  vpc_id      = module.vpc.vpc_id
-  subnet_ids  = module.vpc.intra_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.intra_subnets
 
   instance_class = var.db_instance_class
   multi_az       = var.db_multi_az
