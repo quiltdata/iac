a5835d85942574455b5c896fee08db5b75e4e24c
Aneesh Karve <aneesh.karve@gmail.com>
2023-12-21 15:33:28 -0700

    uppercase

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -14,7 +14,7 @@ variable "cidr" {
   nullable = false
   validation {
     condition     = split("/", var.cidr)[1] < 22
-    error_message = "cidr prefix should allow for at least 1,024 IP addresses"
+    error_message = "CIDR prefix should allow for at least 1,024 IP addresses"
   }
 }
 
