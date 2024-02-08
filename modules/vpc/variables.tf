e494957f1436d8580ce201233f81fea5c2578b1b
Aneesh Karve <akarve@users.noreply.github.com>
2024-02-07 19:40:09 -0700

    Allow smaller CIDR ranges (#48)

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -13,8 +13,8 @@ variable "cidr" {
   type     = string
   nullable = false
   validation {
-    condition     = split("/", var.cidr)[1] <= 22
-    error_message = "CIDR prefix should allow for at least 1,024 IP addresses"
+    condition     = split("/", var.cidr)[1] <= 24
+    error_message = "CIDR prefix should allow for at least 256 IPv4 addresses"
   }
 }
 
