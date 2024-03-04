c521293c0bdb4d6ac2d0d0e91ee6a28609a58633
Aneesh Karve <akarve@users.noreply.github.com>
2024-03-04 12:18:43 -0700

    Remove bad provider pin in example (#55)

--- modules/cnames/main.tf
+++ modules/cnames/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"  # specify the version according to your requirement
+      version = "~> 5.0"
     }
   }
 }
