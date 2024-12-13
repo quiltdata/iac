f1c1b16f89c9b10b67d3268812703619632a5f38
Sergey Fedoseev <fedoseev.sergey@quiltdata.io>
2024-12-13 17:50:52 +0100

    Really use on_failure variable, add on_failure to lifecycle.ignore_change so old stacks are not replaced (#77)

--- modules/quilt/main.tf
+++ modules/quilt/main.tf
@@ -133,5 +133,11 @@ resource "aws_cloudformation_stack" "stack" {
     create = var.create_timeout
   }
 
-  on_failure = "ROLLBACK"
+  on_failure = var.on_failure
+
+  lifecycle {
+    ignore_changes = [
+      on_failure,
+    ]
+  }
 }
